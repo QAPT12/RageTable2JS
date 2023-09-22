0dcda9a05afb03cb0299dfb461bcb99fb902659a
Tuck <Quintin.Tuck@k202domain.com>
2023-09-22 11:04:41 -0230

    functional

--- rageTable.js
+++ rageTable.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+    
     var ragearray = [];
     
     document.getElementById('loadtext').addEventListener('click', function() {
@@ -7,24 +7,15 @@ document.addEventListener("DOMContentLoaded", function() {
         
         filereader.onload = function() {
             var result = filereader.result;
-            document.getElementById('output').innerText = result;
+            ragearray = result.split('\n');
+            
         };
 
-        // filereader.onload = function() {
-        //     var result = filereader.result;
-        //     result.split('\n').array.forEach(element => {
-        //     ragearray.push(element);
-        //     });
-            
-
-        
-        
         filereader.readAsText(document.getElementById("inputfile").files[0]);
     });
 
     document.getElementById("ragebutton").addEventListener("click", function() {
-        document.getElementById("effectdisplay").innerText = "Result";
-        // document.getElementById("output").innerText = ragearray[3];
-
+        document.getElementById("effectdisplay").innerText = ragearray[Math.floor(Math.random() * (ragearray.length))];
+        
     });
 });
