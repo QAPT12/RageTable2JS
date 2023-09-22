d583b32b625e8a4de33d825344e945fde3c67a93
Tuck <Quintin.Tuck@k202domain.com>
2023-09-22 13:18:22 -0230

    visual change + result if file loads correctly or not

--- rageTable.js
+++ rageTable.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var ragearray = [];
     
     document.getElementById('inputfile').addEventListener('change', function() {
+        try{
         var filereader = new FileReader();
         
         filereader.onload = function() {
@@ -12,6 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
         };
 
         filereader.readAsText(document.getElementById("inputfile").files[0]);
+        document.getElementById("result").innerHTML = '<p style="color:green;">File loaded successfully!</p>';
+        } catch(error) {
+            document.getElementById("result").innerHTML = '<p style="color:red;">Error loading file.</p>';
+        }
     });
 
     document.getElementById("ragebutton").addEventListener("click", function() {
