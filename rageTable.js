ac698fa8940bf2607cef7125a47f1ec56dfd7f01
unknown <tuckquintin@hotmail.com>
2023-09-22 10:15:33 -0230

    trying to get the array and for loops to work

--- rageTable.js
+++ rageTable.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    var ragearray = [];
     
     document.getElementById('loadtext').addEventListener('click', function() {
         var filereader = new FileReader();
@@ -7,7 +9,22 @@ document.addEventListener("DOMContentLoaded", function() {
             var result = filereader.result;
             document.getElementById('output').innerText = result;
         };
+
+        // filereader.onload = function() {
+        //     var result = filereader.result;
+        //     result.split('\n').array.forEach(element => {
+        //     ragearray.push(element);
+        //     });
+            
+
+        
         
         filereader.readAsText(document.getElementById("inputfile").files[0]);
+    });
+
+    document.getElementById("ragebutton").addEventListener("click", function() {
+        document.getElementById("effectdisplay").innerText = "Result";
+        // document.getElementById("output").innerText = ragearray[3];
+
     });
 });
